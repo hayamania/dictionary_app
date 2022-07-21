d8f6c91349112da035a567656d361aa21849014e
Chie Hym <hayama.chie@gmail.com>
2022-07-21 14:22:02 +0800

    added Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import React from "react";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <p>This is dictionary app.</p>
+      <div className="container">
+        <header>Dictionary app</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>
+            This is coded by Chie HYM and{" "}
+            <a href="https://clinquant-lolly-a74fa2.netlify.app/">
+              open-sorced
+            </a>{" "}
+            on Netlify
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary text-center">
+      <div className="container m-3">
+        <h1>Dictionary</h1>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    </div>
+  );
+}
