737cfc0263662fb365fe38da77987b2ee9277862
Chie Hym <hayama.chie@gmail.com>
2022-07-28 15:06:03 +0800

    added visualisation in css

--- src/Result.js
+++ src/Result.js
@@ -8,17 +8,19 @@ export default function Result(props) {
   if (props.result) {
     return (
       <div className="Result">
-        <span className="Result-word me-2">{props.result.word}</span>
-        <span className="Result-phonetic"> {props.result.phonetic}</span>
-        <Audio data={props.result.phonetics[0].audio} />
+        <section>
+          <span className="Result-word me-2">{props.result.word}</span>
+          <span className="Result-phonetic"> {props.result.phonetic}</span>
+          <Audio data={props.result.phonetics[0].audio} />
+        </section>
         {props.result.meanings.map(function (meaning, index) {
           return (
-            <div key={index} className="mb-1">
+            <section key={index} className="mb-1">
               <span className="partOfSpeech">{meaning.partOfSpeech}</span>
               <br />
               <Definition definitions={meaning.definitions} />
               <Synonym synonyms={meaning.synonyms} />
-            </div>
+            </section>
           );
         })}
       </div>
